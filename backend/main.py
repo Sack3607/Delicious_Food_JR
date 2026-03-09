4e11bec77d9dd8a3f742883b6fce82e650fa4d79
Sack3607 <Sack3607>
2026-03-08 22:27:48 -0500

    Cambiso nuevos

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+
+app = Flask(
+    __name__,
+    static_folder="../frontend",  # toda la carpeta frontend
+    static_url_path=""            # sirve directamente desde la raíz
+)
+
 
 @app.route("/")
 def home():
