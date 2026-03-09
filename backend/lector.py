76c50ac446a9a4064baca3776911a015d5f12066
Sack3607 <Sack3607>
2026-03-09 17:54:22 -0500

    Actualizado_porque3

--- backend/lector.py
+++ backend/lector.py
@@ -1,6 +1,7 @@
+
+from pathlib import Path
+
 def cargar_conocimiento():
-
-    with open("datos.txt", "r", encoding="utf-8") as archivo:
-        texto = archivo.read()
-
-    return texto
+    ruta = Path(__file__).parent / "datos.txt"  # busca dentro de la misma carpeta que lector.py
+    with open(ruta, "r", encoding="utf-8") as f:
+        return f.read()
